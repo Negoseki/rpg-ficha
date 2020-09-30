6c7fa1ac2b5e728bb8da2a0780ef90254cb54c0e
Negoseki <lucas.negoseki@hotmail.com>
2020-09-30 00:20:40 -0300

    Ajustando vetor e aumentando para android 21

--- app/src/main/java/br/pucpr/appdev/duffeck/rpg_ficha/Controller/MainActivity.kt
+++ app/src/main/java/br/pucpr/appdev/duffeck/rpg_ficha/Controller/MainActivity.kt
@@ -1,15 +1,15 @@
 package br.pucpr.appdev.duffeck.rpg_ficha.Controller
 
-import android.opengl.Visibility
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import br.pucpr.appdev.duffeck.rpg_ficha.Model.DataStore
 import br.pucpr.appdev.duffeck.rpg_ficha.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,11 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        DataStore.setContext(this)
+
         navigationView = findViewById(R.id.navigationView);
 
         val navController = findNavController(R.id.fragNavHostFragment)
         val drawer: DrawerLayout = findViewById(R.id.layDrawer)
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_habilidades, R.id.navigation_vida, R.id.navigation_ataque), drawer)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_habilidades,
+                R.id.navigation_vida,
+                R.id.navigation_ataque
+            ), drawer
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navigationView?.setupWithNavController(navController)
 
