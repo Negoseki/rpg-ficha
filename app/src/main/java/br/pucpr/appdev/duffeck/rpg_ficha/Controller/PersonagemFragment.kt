793d2c7b87bd70408ee6d84aff591527757741fa
Duffeck <lsro@gft.com>
2020-10-03 02:20:30 -0300

    Loading spinner

--- app/src/main/java/br/pucpr/appdev/duffeck/rpg_ficha/Controller/PersonagemFragment.kt
+++ app/src/main/java/br/pucpr/appdev/duffeck/rpg_ficha/Controller/PersonagemFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
+import android.widget.ProgressBar
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -20,6 +21,7 @@ class PersonagemFragment : Fragment() {
 
     private var columnCount = 1
     private var lista: RecyclerView? = null
+    private var spinner: ProgressBar? = null ;
 
 
     override fun onCreateView(
@@ -30,6 +32,7 @@ class PersonagemFragment : Fragment() {
         (context as MainActivity).toggleToolbar(false)
         val view = inflater.inflate(R.layout.fragment_personagem_list, container, false)
         lista = view.findViewById(R.id.list)
+        spinner = view.findViewById(R.id.progressBar1);
         val gestureDetector =
             GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
                 override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
@@ -74,8 +77,10 @@ class PersonagemFragment : Fragment() {
 
         })
 
+        spinner?.setVisibility(View.VISIBLE);
         DataStore.getAllItems().subscribe {
             with(lista!!) {
+                spinner?.setVisibility(View.GONE);
                 layoutManager = when {
                     columnCount <= 1 -> LinearLayoutManager(context)
                     else -> GridLayoutManager(context, columnCount)
